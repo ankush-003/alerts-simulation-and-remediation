747150fbe13f9375b3aa0622b87d8ad6164b9a53
ankush003 <anshhv2003@gmail.com>
2024-03-18 07:26:45 +0530

    updated ui

--- dashboard/app/realtime/columns.tsx
+++ dashboard/app/realtime/columns.tsx
@@ -2,6 +2,18 @@ import { ColumnDef } from "@tanstack/react-table"
 import { z } from "zod"
 import { cn } from "@/lib/utils";
 import { parse, format, addDays, subMonths } from 'date-fns';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
 
 const dateSchema = 'yyyy-MM-dd HH:mm:ss'
 
@@ -48,7 +60,7 @@ export const columns: ColumnDef<Alert>[] = [
         header: "Created At",
         accessorKey: "created_at",
         cell: ({ row }) => {
-            const dateString:string = row.getValue("created_at")
+            const dateString: string = row.getValue("created_at")
             const date = parse(dateString, dateSchema, new Date())
 
             return (
@@ -63,19 +75,50 @@ export const columns: ColumnDef<Alert>[] = [
         accessorKey: "status",
     },
     {
-        header: "Num Goroutine",
-        accessorKey: "runtime_metrics.num_goroutine",
-    },
-    {
-        header: "Allocated Mem",
-        accessorKey: "runtime_metrics.allocated_mem_bytes",
-    },
-    {
-        header: "Total Allocated Mem",
-        accessorKey: "runtime_metrics.total_allocated_mem_bytes",
-    },
-    {
-        header: "Sys Mem",
-        accessorKey: "runtime_metrics.sys_mem_bytes",
-    },
+        header: "Details",
+        cell: ({ row }) => {
+            const dateString: string = row.getValue("created_at")
+            const date = parse(dateString, dateSchema, new Date())
+            const runtime_metrics: Alert["runtime_metrics"] = row.getValue("runtime_metrics")
+            console.log(runtime_metrics)
+
+            return (
+               <div>
+                    {row.getValue("status") === "open" ? (
+                        <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button variant="outline">Acknowledge</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>Are you sure you want to acknowledge this alert?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Alert ID: {row.getValue("id")}
+                                Description : {row.getValue("description")}
+                            
+                                {/* Node Runtime Metrics:
+                                <ul>
+                                    <li>Number of Goroutines: {runtime_metrics.num_goroutine}</li>
+                                    <li>Allocated Memory: {runtime_metrics.allocated_mem_bytes}</li>
+                                    <li>Total Allocated Memory: {runtime_metrics.total_allocated_mem_bytes}</li>
+                                    <li>System Memory: {runtime_metrics.sys_mem_bytes}</li>
+                                </ul> */}
+                                Created At: {format(date, "yyyy-MM-dd HH:mm:ss")}
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction>Continue</AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    ) : (
+                        <Button disabled className="bg-green-100">
+                            Acknowledged
+                        </Button>
+                    )}
+               </div>
+            )
+        }
+    }
 ]
