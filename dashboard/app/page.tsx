d4658590b448828d34c49e155c29bbd2cd3b7405
Vibhav1612 <vibhav.vasudevan@gmail.com>
2024-04-08 22:29:00 +0530

    basic prototype of login page done

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -2,6 +2,8 @@
 import { TypewriterEffect } from "../components/ui/typewriter-effect";
 import { AlertTriangle } from "lucide-react";
 import { useEffect, useRef } from "react";
+import Link from "next/link";
+
 import Lottie from "lottie-react";
 import alertAnimation from "../public/alertAnimation.json";
 
@@ -40,9 +42,12 @@ export default function Home() {
         <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-lg">
           Join now
         </button>
+        <Link href = "/signup">
         <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-lg">
           Signup
         </button>
+        </Link>
+
       </div>
 
     </div>
