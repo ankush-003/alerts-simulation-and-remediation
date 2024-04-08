f203865585fc4b008f6afd86b7a9f37fffd1f462
Abhay NJ <abhayjoshi029@gmail.com>
2024-04-08 15:01:23 +0530

    Adding custom Unmarshal method to Alerts

--- middleware/rule_engine_v2/engine/alert_configs.go
+++ middleware/rule_engine_v2/engine/alert_configs.go
@@ -1,6 +1,12 @@
 package rule_engine
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"time"
+)
 
 // Implements RuleInput interface
 // Holds all the input to rule engine parameters
@@ -17,6 +23,69 @@ type AlertInput struct {
 
 func (alert *AlertInput) DataKey() string {
 	return "AlertInput"
+}
+
+func (alert *AlertInput) Unmarshal(obj []byte) error {
+	var data map[string]interface{}
+	err := json.Unmarshal(obj, &data)
+	if err != nil {
+		return err
+	}
+	paramsData := data["params"].(map[string]interface{})
+	paramsType := data["category"].(string)
+	alert.Category = data["category"].(string)
+	alert.ID = data["id"].(string)
+	alert.Source = data["source"].(string)
+	alert.Handled = data["handled"].(bool)
+	alert.Origin = data["origin"].(string)
+	alert.CreatedAt = data["createdAt"].(time.Time)
+	switch paramsType {
+	case "Memory":
+		var memory Memory
+		if err := unmarshalParam(memory, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &memory
+	case "CPU":
+		var cpu CPU
+		if err := unmarshalParam(cpu, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &cpu
+	case "Disk":
+		var disk Disk
+		if err := unmarshalParam(disk, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &disk
+	case "Network":
+		var network Network
+		if err := unmarshalParam(network, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &network
+	case "Power":
+		var power Power
+		if err := unmarshalParam(power, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &power
+	case "Applications":
+		var apps Applications
+		if err := unmarshalParam(apps, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &apps
+	case "Security":
+		var security Security
+		if err := unmarshalParam(security, paramsData); err != nil {
+			return err
+		}
+		alert.Params = &security
+	default:
+		return errors.New("WRONG PARAM INPUT TYPE")
+	}
+	return nil
 }
 
 type AlertOutput struct {
@@ -39,10 +108,6 @@ type Memory struct {
 	SwapUsage  uint `json:"swapUsage"`
 }
 
-func (mem *Memory) DataKey() string {
-	return "MemInput"
-}
-
 // CPU:
 // Utilization (%)
 // Temperature (°C)
@@ -52,10 +117,6 @@ func (mem *Memory) DataKey() string {
 type CPU struct {
 	Utilization uint `json:"utilization"`
 	Temperature uint `json:"temperature"`
-}
-
-func (cpu *CPU) DataKey() string {
-	return "CpuInput"
 }
 
 // Disk:
@@ -107,4 +168,58 @@ type Security struct {
 	FailedLogins   uint `json:"failedLogins"`
 	SuspectedFiles uint `json:"suspectedFiles"`
 	IDSEvents      uint `json:"idsEvents"`
+}
+
+func (cpu *CPU) DataKey() string {
+	return "CpuInput"
+}
+
+func (mem *Memory) DataKey() string {
+	return "MemInput"
+}
+
+func (disk *Disk) DataKey() string {
+	return "DiskInput"
+}
+
+func (net *Network) DataKey() string {
+	return "NetInput"
+}
+
+func (pow *Power) DataKey() string {
+	return "PowerInput"
+}
+
+func (apps *Applications) DataKey() string {
+	return "AppInput"
+}
+
+func (sec *Security) DataKey() string {
+	return "SecInput"
+}
+
+func unmarshalParam(ptr interface{}, paramsData map[string]interface{}) error {
+	structType := reflect.TypeOf(ptr).Elem()
+	fmt.Println(structType)
+	paramsField, found := structType.FieldByName("Params")
+	if !found {
+		return errors.New("struct has no field named Params")
+	}
+	fmt.Println(paramsField.Name)
+	expectedType := paramsField.Type.Elem()
+
+	paramsValue := reflect.New(expectedType).Interface()
+
+	paramsBytes, err := json.Marshal(paramsData)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(paramsBytes, paramsValue)
+	if err != nil {
+		return err
+	}
+
+	// Set the value of the Params field in the original struct
+	reflect.ValueOf(ptr).Elem().FieldByName("Params").Set(reflect.ValueOf(paramsValue))
+	return nil
 }
--- middleware/sim/kafka/consumer.go
+++ middleware/sim/kafka/consumer.go
@@ -3,7 +3,6 @@ package kafka
 import (
 	rule_engine "github.com/ankush-003/alerts-simulation-and-remediation/middleware/rule_engine_v2/engine"
 
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -59,39 +58,8 @@ consumerLoop:
 	for {
 		select {
 		case msg := <-partitionConsumer.Messages():
-			var data map[string]interface{}
-			err := json.Unmarshal(msg.Value, &data)
-			if err != nil {
-				c.Logger.Printf("Error unmarshalling alert: %s\n", err)
-				continue
-			}
-			fmt.Println(data)
-			paramsData := data["params"].(map[string]interface{})
-			paramsType := data["category"].(string)
-
 			var parsedAlert rule_engine.AlertInput
-			parsedAlert.Category = data["category"].(string)
-			parsedAlert.ID = data["id"].(string)
-			parsedAlert.Source = data["source"].(string)
-			parsedAlert.Handled = data["handled"].(bool)
-			switch paramsType {
-			case "Memory":
-				var memory rule_engine.Memory
-				paramsBytes, err := json.Marshal(paramsData) // Convert map to JSON bytes
-				if err != nil {
-					c.Logger.Printf("Error unmarshalling alert: %s\n", err)
-					continue
-				}
-				err = json.Unmarshal(paramsBytes, &memory)
-				if err != nil {
-					c.Logger.Printf("Error unmarshalling alert: %s\n", err)
-					continue
-				}
-				parsedAlert.Params = &memory
-			case "CPU":
-			default:
-				c.Logger.Printf("Error unmarshalling alert: %s\n", err)
-			}
+			parsedAlert.Unmarshal(msg.Value)
 			// err = json.Unmarshal(msg.Value, &alert)
 			// c.Logger.Printf("Consumed alert: %v\n", parsedAlert)
 			alertsChan <- parsedAlert
