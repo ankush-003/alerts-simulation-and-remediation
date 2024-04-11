8d25af7b48c48ce5b2db12b103b884cb8eecf0c9
Abhay NJ <abhayjoshi029@gmail.com>
2024-04-11 16:45:27 +0530

    Merged Mailserver

---
+++ middleware/rule_engine_v2/mailserver/mailserver.go
@@ -0,0 +1,48 @@
+package mailserver
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ankush-003/alerts-simulation-and-remediation/middleware/sim/alerts"
+
+	gomail "gopkg.in/mail.v2"
+)
+
+func SendEmail(input alerts.AlertInput, output alerts.AlertOutput, mail string) error {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", "alertssim@gmail.com")
+	mailer.SetHeader("To", mail)
+	mailer.SetHeader("Subject", "Alerts for Node ", input.Origin)
+	// fmt.Println("In mailserver", alert)
+	// fmt.Println(Severity, Remedy)
+	// fmt.Println(ID, Category, CreatedAt, Handled, Source, Origin, Severity, Remedy)
+	body := fmt.Sprintf(
+		"%s: %s, %s: %s, %s: %s, %s: %t, %s: %s, %s: %s, %s: %s, %s: %s",
+		"ID", input.ID,
+		"Category", input.Category,
+		"CreatedAt", input.CreatedAt,
+		"Handled", input.Handled,
+		"Source", input.Source,
+		"Origin", input.Origin,
+		"Severity", output.Severity,
+		"Remedy", output.Remedy,
+	)
+	// // fmt.Println(body)
+	mailer.SetBody("text/plain", body)
+
+	// // SMTP server settings
+	// fmt.Println(os.Getenv("APP_PWD"))
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, "alertssim@gmail.com", os.Getenv("APP_PWD"))
+	// fmt.Println(dialer)
+
+	err := dialer.DialAndSend(mailer)
+
+	if err != nil {
+		fmt.Println("Email sending failed!")
+		return fmt.Errorf("error sending email: %v", err)
+	}
+	fmt.Println("Email sent successfully!")
+	return nil
+
+}
