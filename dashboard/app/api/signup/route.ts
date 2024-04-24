582b4b4f079c1e499d0ba9ff513aa34368fc5d8a
ankush003 <anshhv2003@gmail.com>
2024-04-24 19:46:16 +0530

    commented bcrypt code usage

--- dashboard/app/api/signup/route.ts
+++ dashboard/app/api/signup/route.ts
@@ -1,32 +1,32 @@
-import { NextResponse } from 'next/server';
-import clientPromise from "../../../lib/mongodb";
-import { hash } from 'bcrypt';
+// import { NextResponse } from 'next/server';
+// import clientPromise from "../../../lib/mongodb";
+// import { hash } from 'bcrypt';
 
-interface UserDetails {
-  name: string;
-  email: string;
-  password: string;
-}
+// interface UserDetails {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
 
-export async function POST(req: Request) {
-  try {
-    const { name, email, password } = await req.json() as UserDetails;
-    const hashedPassword = await hash(password, 10);
-    const client = await clientPromise;
-    const db = client.db("AlertSimAndRemediation");
-    const usersCollection = db.collection("Users");
-    const result = await usersCollection.insertOne({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    if (result.acknowledged) {
-      return NextResponse.json({ message: 'Signup successful' }, { status: 200 });
-    } else {
-      return NextResponse.json({ message: 'Signup failed' }, { status: 500 });
-    }
-  } catch (e) {
-    console.error(e);
-    return NextResponse.json({ message: 'An error occurred' }, { status: 500 });
-  }
-}
+// export async function POST(req: Request) {
+//   try {
+//     const { name, email, password } = await req.json() as UserDetails;
+//     const hashedPassword = await hash(password, 10);
+//     const client = await clientPromise;
+//     const db = client.db("AlertSimAndRemediation");
+//     const usersCollection = db.collection("Users");
+//     const result = await usersCollection.insertOne({
+//       name,
+//       email,
+//       password: hashedPassword,
+//     });
+//     if (result.acknowledged) {
+//       return NextResponse.json({ message: 'Signup successful' }, { status: 200 });
+//     } else {
+//       return NextResponse.json({ message: 'Signup failed' }, { status: 500 });
+//     }
+//   } catch (e) {
+//     console.error(e);
+//     return NextResponse.json({ message: 'An error occurred' }, { status: 500 });
+//   }
+// }
