feaa6242673ef6009e7063be1aa1edaaba1fbc5f
ankush003 <anshhv2003@gmail.com>
2024-04-24 19:55:22 +0530

    shifted argon2

--- dashboard/app/api/login/route.ts
+++ dashboard/app/api/login/route.ts
@@ -1,35 +1,36 @@
-// import { NextResponse } from 'next/server';
-// import clientPromise from "@/lib/mongodb";
-// // import { compare } from 'bcrypt';
+import { NextResponse } from 'next/server';
+import clientPromise from "@/lib/mongodb";
+// import { compare } from 'bcrypt';
+import { verify } from 'argon2';
 
-// interface UserCredentials {
-//   email: string;
-//   password: string;
-// }
+interface UserCredentials {
+  email: string;
+  password: string;
+}
 
-// export async function POST(req: Request) {
-//   try {
-//     const { email, password } = await req.json() as UserCredentials;
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json() as UserCredentials;
 
-//     const client = await clientPromise;
-//     const db = client.db("AlertSimAndRemediation");
-//     const usersCollection = db.collection("Users");
+    const client = await clientPromise;
+    const db = client.db("AlertSimAndRemediation");
+    const usersCollection = db.collection("Users");
 
-//     const user = await usersCollection.findOne({ email });
+    const user = await usersCollection.findOne({ email });
 
-//     if (!user) {
-//       return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
-//     }
+    if (!user) {
+      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
 
-//     const isPasswordValid = await compare(password, user.password);
+    const isPasswordValid = await verify(user.password, password);
 
-//     if (!isPasswordValid) {
-//       return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
-//     }
+    if (!isPasswordValid) {
+      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
 
-//     return NextResponse.json({ message: 'Login successful' }, { status: 200 });
-//   } catch (e) {
-//     console.error(e);
-//     return NextResponse.json({ message: 'An error occurred' }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ message: 'Login successful' }, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ message: 'An error occurred' }, { status: 500 });
+  }
+}
