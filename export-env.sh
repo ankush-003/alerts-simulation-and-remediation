8f7e0c9d94c008f588358a558d34b80c093d8db3
Ankush H V <anshhv2003@gmail.com>
2024-05-25 18:51:07 +0530

    removed export-env

--- export-env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-export MONGODB_URI="mongodb+srv://alertssim:RRdxdKTHt99cEnWw@cluster0.aenmhq0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-export KAFKA_BROKER="active-boar-11578-us1-kafka.upstash.io:9092"
-export KAFKA_USERNAME="YWN0aXZlLWJvYXItMTE1NzgkSad0k7Y1TR2gDOgfvRl8R5Xs7vFQITViGMqTLl8"
-export KAFKA_PASSWORD="YTFmMTdhNDAtMWI3Mi00ZTcyLWE1MjItYzJlMjgzOTMxZjU3"
-export REDIS_ADDR="redis://default:ybaCdWLadAzqrb2qXO7QhKgjiDL3pXZ5@redis-16652.c212.ap-south-1-1.ec2.cloud.redislabs.com:16652"
