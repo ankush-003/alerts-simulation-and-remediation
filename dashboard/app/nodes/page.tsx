1de33c77b356d507dc2b0dcbb89356c34856743b
Ankush H V <anshhv2003@gmail.com>
2024-05-30 01:57:47 +0530

    updated webui

--- dashboard/app/nodes/page.tsx
+++ dashboard/app/nodes/page.tsx
@@ -70,7 +70,9 @@ export default function Nodes() {
     <>
       {Object.keys(nodes).length === 0 && (
         <div className="text-center">
-          <h2 className="text-xl font-semibold text-center text-red-500">No nodes found</h2>
+          <h2 className="text-xl font-semibold text-center text-red-500">
+            No nodes found
+          </h2>
           <p className="text-lg text-center">
             Please make sure the nodes are running
           </p>
@@ -83,83 +85,81 @@ export default function Nodes() {
           console.table(node);
           const isDown = node.status === "down";
           return (
-              <Card className="border-2 border-gray-200 p-4" key={node.nodeID}>
-                <CardContent className="grid gap-4">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                      {/* <Computer className="w-6 h-6 text-gray-500 dark:text-gray-400" /> */}
-                      <h3 className="font-bold text-lg">
+            <Card className="border-2 border-gray-200 p-4" key={node.nodeID}>
+              <CardContent className="grid gap-4">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    {/* <Computer className="w-6 h-6 text-gray-500 dark:text-gray-400" /> */}
+                    <h3 className="font-bold text-lg">
                       <span className="text-red-500">{node.nodeID}</span>
-                      </h3>
-                    </div>
-                    {
-                      isDown ? (
-                        <Computer className="w-6 h-6 text-red-500" />
-                      ) : (
-                        <Computer className="w-6 h-6 text-green-500" />
-                      )
-                    }
+                    </h3>
                   </div>
-                  <div className="grid gap-2">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <Cpu className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-                        <span className="text-gray-500 dark:text-gray-400">
-                          CPU Usage
-                        </span>
-                      </div>
-                      {node.cpuUsage === '-1' ?? (
-                        <span className="font-medium text-red-500">
-                          Not connected to Prometheus
-                        </span>
-                      )}
-                    </div>
-                    {node.cpuUsage === '-1' ? (
-                      <Progress value={0} />
-                    ) : (
-                      <Progress value={parseFloat(node.cpuUsage)} />
-                    )}
-                  </div>
-                  <div className="grid gap-2">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <MemoryStick className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-                        <span className="text-gray-500 dark:text-gray-400">
-                          RAM Usage
-                        </span>
-                      </div>
-                      {node.ramUsage === '-1' ?? (
-                        <span className="font-medium text-red-500">
-                          Not connected to Prometheus
-                        </span>
-                      )}
-                    </div>
-                    {node.ramUsage === '-1' ? (
-                      <Progress value={0} />
-                    ) : (
-                      <Progress value={parseFloat(node.ramUsage)} />
-                    )}
-                  </div>
+                  {isDown ? (
+                    <Computer className="w-6 h-6 text-red-500" />
+                  ) : (
+                    <Computer className="w-6 h-6 text-green-500" />
+                  )}
+                </div>
+                <div className="grid gap-2">
                   <div className="flex items-center justify-between">
                     <div className="flex items-center gap-2">
                       <Cpu className="w-4 h-4 text-gray-500 dark:text-gray-400" />
                       <span className="text-gray-500 dark:text-gray-400">
-                        Threads
+                        CPU Usage
                       </span>
                     </div>
-                    {node.numGoroutine == "-1" ?? (
+                    {node.cpuUsage === "-1" ?? (
                       <span className="font-medium text-red-500">
                         Not connected to Prometheus
                       </span>
                     )}
                   </div>
-                  {node.numGoroutine == "-1" ? (
-                    <span className="font-medium">0</span>
+                  {node.cpuUsage === "-1" ? (
+                    <Progress value={0} />
                   ) : (
-                    <span className="font-medium">{node.numGoroutine}</span>
+                    <Progress value={parseFloat(node.cpuUsage)} />
                   )}
-                </CardContent>
-              </Card>
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <MemoryStick className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                      <span className="text-gray-500 dark:text-gray-400">
+                        RAM Usage
+                      </span>
+                    </div>
+                    {node.ramUsage === "-1" ?? (
+                      <span className="font-medium text-red-500">
+                        Not connected to Prometheus
+                      </span>
+                    )}
+                  </div>
+                  {node.ramUsage === "-1" ? (
+                    <Progress value={0} />
+                  ) : (
+                    <Progress value={parseFloat(node.ramUsage)} />
+                  )}
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    <Cpu className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                    <span className="text-gray-500 dark:text-gray-400">
+                      Threads
+                    </span>
+                  </div>
+                  {node.numGoroutine == "-1" ?? (
+                    <span className="font-medium text-red-500">
+                      Not connected to Prometheus
+                    </span>
+                  )}
+                </div>
+                {node.numGoroutine == "-1" ? (
+                  <span className="font-medium">0</span>
+                ) : (
+                  <span className="font-medium">{node.numGoroutine}</span>
+                )}
+              </CardContent>
+            </Card>
           );
         })}
       </section>
